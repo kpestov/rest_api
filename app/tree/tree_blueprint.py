48c481a8aac5c555165f2ea04f4038d551954596
kpestov <kpestov91@gmail.com>
2018-12-21 22:28:33 +0500

    aded search

--- app/tree/tree_blueprint.py
+++ app/tree/tree_blueprint.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask import render_template
 from models import TreeNode
+from flask import request
 
 
 tree = Blueprint('tree', __name__, template_folder='templates')
@@ -8,12 +9,20 @@ tree = Blueprint('tree', __name__, template_folder='templates')
 
 @tree.route('/')
 def index():
-    items = TreeNode.query.all()
+
+    q = request.args.get('q')
+
+    if q:
+        elements = TreeNode.query.filter(TreeNode.name.contains(q) | TreeNode.body.contains(q)).all()
+    else:
+        elements = TreeNode.query.all()
+
     root = TreeNode.query.filter(TreeNode.parent_id.is_(None)).first()
     root_child = TreeNode.query.filter(TreeNode.parent_id == root.id).first()
     subnodes = TreeNode.query.filter(TreeNode.body.isnot(None)).all()
+
     return render_template('tree/index.html',
-                           items=items,
+                           elements=elements,
                            root=root,
                            root_child=root_child,
                            subnodes=subnodes
