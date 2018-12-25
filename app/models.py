9be2e1da68f77e13dae630039401458b9f0f633b
Kirill Pestov <34282471+kpestov@users.noreply.github.com>
2018-12-25 21:43:48 +0500

    Update models.py
    removed commented code

--- app/models.py
+++ app/models.py
@@ -1,12 +1,6 @@
 import re
 from sqlalchemy.orm.collections import attribute_mapped_collection
-
 from app import db
-
-# from flask_sqlalchemy import SQLAlchemy
-# from flask import Flask
-# app = Flask(__name__)
-# db = SQLAlchemy(app)
 
 
 def slugify(s):
